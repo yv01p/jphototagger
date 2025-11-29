beb14a6dadaf13c5d2c4e47b78a05afa846c824c
yv01p <yv01p@outlook.com>
2025-11-29 00:17:46 +0000

    chore: add HSQLDB Repository module Gradle build

---
+++ Repositories/HSQLDB/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api(project(":API"))
+    api(project(":Domain"))
+    api(project(":Image"))
+    api(project(":Lib"))
+    api(project(":Resources"))
+    api(project(":XMP"))
+
+    // Maven Central
+    api(libs.hsqldb)
+
+    // Local JARs
+    api(files("../../Libraries/org-openide-util-lookup.jar"))
+    api(files("../../Libraries/eventbus.jar"))
+}
+
+sourceSets {
+    main {
+        java.srcDirs("src")
+        resources.srcDirs("src")
+    }
+    test {
+        java.srcDirs("test")
+    }
+}
